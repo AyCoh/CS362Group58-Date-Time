09255dc1008c13eb68113553d1e9ebacf9aaaa46
Ollaradam <97258986+Ollaradam@users.noreply.github.com>
2022-07-28 21:22:07 -0700

    Update tests.py

--- tests.py
+++ tests.py
@@ -7,8 +7,8 @@ class TestCase(unittest.TestCase):
     def test1(self):
         expected = "Hello World"
         self.assertEqual(task.my_func(), expected)
-    def test2(self):
         
+    def test2(self):
         expected = "Goodbye World"
         self.assertNotEqual(task.my_func(), expected)
 
