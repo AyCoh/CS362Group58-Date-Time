ebc4558ef2ae0ae7f6b0268f0c25276d694c6b50
Adam Schneider <schneada@gmail.com>
2022-08-03 21:42:13 -0500

    task.py - Wrote headers and comments for the three required functions.
    tests.py -
    Wrote out tests for each of the 19 examples included in the documentation for project part 2.  More tests will need to be added.

--- task.py
+++ task.py
@@ -1,2 +1,21 @@
-def my_func():
-    return "Hello World"
+def conv_num(num_str):
+    """
+    Takes a string and converts it into a base 10 number which is then returned.
+    Can handle strings representing: integers, floating-point numbers, and hexadecimals with prefix '0x'.
+    """
+    pass
+
+
+def my_datetime(num_sec):
+    """
+    Takes a non-negative integer value that represents number of seconds since Jan 1, 1970 (epoch) and converts to a date
+    to be returned as a string in 'MM-DD-YYYY' format.
+    """
+    pass
+
+
+def conv_endian(num, endian='big'):
+    """
+    Takes an integer value and endian flag then converts and returns as a string.
+    """
+    pass
