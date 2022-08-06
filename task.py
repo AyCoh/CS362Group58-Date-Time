d621fec447398605c692498ea4f9bcc60368d3a9
Adam Schneider <schneada@gmail.com>
2022-08-06 13:08:13 -0500

    task.py - Added tests 20 and 21 for conv_num to test a couple more hex cases
    tests.py -
    modified conv_num() to handle hexadecimal strings. added conv_hex helper to be called by conv_num

--- task.py
+++ task.py
@@ -1,7 +1,25 @@
+def conv_hex(num_str):
+    """
+    Takes passed string that has been trimmed of '0x' or '-0x' prefix and returns the positive integer value.
+    If the passed value contains invalid digits, returns None. Called by conv_num().
+    """
+    integer_val = 0
+    hex_digits = {'0': 0, '1': 1, '2': 2, '3': 3, '4': 4, '5': 5, '6': 6, '7': 7, '8': 8, '9': 9, 'A': 10, 'B': 11,
+                  'C': 12, 'D': 13, 'E': 14, 'F': 15}
+    hex_exponent = len(num_str) - 1
+    for digit in num_str:
+        if digit in hex_digits:
+            integer_val += hex_digits[digit] * 16 ** hex_exponent
+            hex_exponent -= 1
+        else:
+            return None
+    return integer_val
+
+
 def conv_num(num_str):
     """
     Takes a string and converts it into a base 10 number which is then returned.
-    Can handle strings representing: integers, floating-point numbers, and hexadecimals with prefix '0x'.
+    Can handle strings representing: integers, floating-point numbers, and integer hexadecimals with prefix '0x'.
     """
     integer_value = 0
     fraction_value = 0
@@ -16,6 +34,17 @@ def conv_num(num_str):
     upper = 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', \
             'U', 'V', 'W', 'X', 'Y', 'Z'
 
+    # Check if num_str is positive hexadecimal and call conversion method
+    if num_str[:2] == '0x':
+        return conv_hex(num_str[2:])
+
+    # Check if num_str is negative hexadecimal and call conversion method
+    if num_str[:3] == '-0x':
+        integer_value = conv_hex(num_str[3:])
+        if integer_value is not None:
+            return integer_value * -1
+        return integer_value
+    
     for digit in num_str:
         if digit in low or digit in upper:
             return None
